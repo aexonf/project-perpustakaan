86b471c8fed89f5705db6dacd6d90e47611a8ac8
AvinFajarF <avinfajar6@gmail.com>
2023-11-22 14:07:10 +0700

    feat: added router for home controller

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +14,13 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::prefix("/")->group(function() {
+
+    //  route untuk HOME
+    Route::controller(HomeController::class)->group(function() {
+        Route::get("/", "index");
+        Route::get("/{id}", "detail");
+    });
+
+
 });
