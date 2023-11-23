c05c2d56e79a1495228367b33cd6db0bb1cd995f
FatsaYahdi <namadepannamabelakang10298318@gmail.com>
2023-11-23 20:29:25 +0700

    fix: missing dependencies

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -6,9 +6,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Simaku</title>
-    {{-- react --}}
-    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
-    @viteReactRefresh
     @vite('resources/js/app.jsx')
     @vite('resources/css/app.css')
     @inertiaHead
