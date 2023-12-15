7fc80a43c8924a485e122929c234cee67e494745
Rigen Maulana <rygen.dev@gmail.com>
2023-12-15 15:51:23 +0700

    fix: change wording logout

--- resources/views/components/elements/app.blade.php
+++ resources/views/components/elements/app.blade.php
@@ -41,9 +41,10 @@
                             <div class="d-sm-none d-lg-inline-block">Hi, User</div>
                         </a>
                         <div class="dropdown-menu dropdown-menu-right">
-                            <a href="#" class="dropdown-item has-icon text-danger"
+                            <a href="#" class="dropdown-item has-icon text-danger d-flex align-items-center"
                                 onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
-                                <i class="fas fa-sign-out-alt">Logout</i>
+                                <i class="fas fa-sign-out-alt"></i>
+                                <span>Keluar</span>
                             </a>
                         </div>
                     </li>
