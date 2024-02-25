0f261858daa695bdfe706eb2d23895297fddaa46
aexonf <echo.fajarf@gmail.com>
2024-02-25 16:32:37 +0700

    Chore: update migration users

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,12 +13,14 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
+            $table->string("id_number")->nullable();
+            $table->string("phone_number")->nullable();
+            $table->string('name')->nullable();
+            $table->string('email')->nullable()->unique();
             $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->enum('role', ["student", "teacher", "librarian"]);
-            $table->enum('status', ['active', 'not_active']);
+            $table->string('password')->nullable();
+            $table->enum('role', ["student", "teacher", "librarian"])->nullable();
+            $table->enum('status', ['active', 'not_active'])->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
